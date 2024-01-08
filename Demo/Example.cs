fceacdf354f47af7f5a25212b8972d06c6419b26
ywkbm <ywkbm1973@gmail.com>
2024-01-08 18:32:04 +0500

    demonstration for singleton added

---
+++ Demo/Example.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Example
+    {
+        public void Print()
+        {
+            Console.WriteLine("example text");
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,13 @@
+﻿using Demo;
+using IoC.Implementation;
+
+var services = new ServiceCollection();
+
+services.AddSingleton<Example>();
+
+var serviceProvider = services.Build();
+var example = serviceProvider.GetService<Example>();
+
+example.Print();
+
+Console.ReadLine();
